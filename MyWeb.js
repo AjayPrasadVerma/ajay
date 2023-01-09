9d1a8a7855b1b3d13f45238da7e0394762395cf6
Ajay <imajvermaofficial@gmail.com>
2023-01-10 00:55:59 +0530

    My Website

---
+++ MyWeb.js
@@ -0,0 +1,24 @@
+
+window.addEventListener("wheel",function(event){
+    if(event.deltaY < 0)
+    {
+        $("#menu-bar").slideDown(1000);
+    }
+    else if(event.deltaY > 0)
+    {
+        
+        $("#menu-bar").slideUp(1000);
+    }
+});
+
+if(window.innerWidth < 1208 && window.innerWidth > 790)
+{
+    $("#right").animate({left : '10px'},500);
+    $("#left").animate({left : '10px'},500);
+}
+else if(window.innerWidth < 790){
+    $("#right").animate({left : '120px'},500);
+    $("#left").animate({left : '-110px'},500);
+    // $("#mountain").css({"width" : window.innerWidth},100);
+
+}
