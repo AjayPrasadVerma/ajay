3d39e423c0c8770fe28553c898e61bc8e64b563f
Ajay <imajvermaofficial@gmail.com>
2023-04-19 22:46:34 +0530

    content updated

--- MyWeb.js
+++ MyWeb.js
@@ -1,14 +1,3 @@
-
-window.addEventListener("wheel", function (event) {
-    if (event.deltaY < 0) {
-        $("#menu-bar").slideDown(1000);
-    }
-    else if (event.deltaY > 0) {
-
-        $("#menu-bar").slideUp(1000);
-    }
-});
-
 if (window.innerWidth < 1208 && window.innerWidth > 790) {
     $("#right").animate({ left: '10px' }, 500);
     $("#left").animate({ left: '10px' }, 500);
